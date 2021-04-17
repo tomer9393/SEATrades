5877ad5879a630f19cafac598a85897ce3f2445c
Tomer.Israel <tomer9393@cs.colman.ac.il>
2021-04-17 21:11:44 +0300

    added categories and locations ( not done)

--- client/client-react/src/components/header/search_bar.js
+++ client/client-react/src/components/header/search_bar.js
@@ -1,8 +1,40 @@
 import React from "react";
+import { useState } from "react";
+import { categories, locations } from "../utils";
+import {
+  TextField,
+  Dropdown,
+  DatePicker,
+  mergeStyleSets,
+} from "@fluentui/react";
+import { homePageSearch } from "../../api/EventAPI";
+import { useHistory } from "react-router-dom";
 
-class SearchBar extends React.Component {
-  render() {
+function SearchBar (props){
+
+  const [searchText, setSearchText] = useState();
+  const [selectedCategory, setSelectedCategory] = useState();
+  const [selectedLocation, setselectedLocation] = useState();
+  const history = useHistory();
+
+  const dropdownOptions1 = [
+    { key: "emptyOption", text: "Select a category" },
+    ...categories.map((category) => ({
+      key: category,
+      text: category,
+    })),
+  ];
+
+  const dropdownOptions2 = [
+    { key: "emptyOption", text: "Select a Location" },
+    ...locations.map((location) => ({
+      key: location,
+      text: location,
+    })),
+  ];
+
     return (
+      <>
       <section className="hero set-bg" data-setbg="/img/hero/bg-homePage.jpg">
         <div className="container">
           <div className="row">
@@ -10,22 +42,29 @@ class SearchBar extends React.Component {
               <div className="hero__text">
                 <div className="section-title" style={{textShadow: '2px 3px #000000'}}>
                   <h2 >Look for tickets to your favorite shows!</h2>
-                  {/* <p>
-                  With SeaTrades you can buy and trade tickets as you wish! 
-                  </p> */}
                 </div>
                 <div className="hero__search__form">
                   <form action="#">
-                    <input type="text" placeholder="Search..." />
-                    <div className="select__option">
-                      <select>
-                        <option value>Choose Categories</option>
-                      </select>
+                  <input type="text"  
+                  placeholder="Enter Artist or Event name..." 
+                  onChange={(_, value) => setSearchText(value || "")}/>
+                    <div className="select__option" >
+                     <Dropdown 
+                      placeholder="Select a category"
+                      options={dropdownOptions1}
+                      onChange={(_, item) => {
+                        setSelectedCategory(item.key);
+                        }}
+                      />
                     </div>
                     <div className="select__option">
-                      <select>
-                        <option value>Choose Location</option>
-                      </select>
+                    <Dropdown 
+                      placeholder="Select a location"
+                      options={dropdownOptions2}
+                      onChange={(_, item) => {
+                        setselectedLocation(item.key);
+                        }}
+                      />
                     </div>
                     <button type="submit">Explore Now</button>
                   </form>
@@ -38,7 +77,7 @@ class SearchBar extends React.Component {
                   </li>
                   <li>
                     <a href="#">
-                      <img src="img/hero/cat-2.png" alt="" /> Music Festivals
+                      <img src="img/hero/cat-2.png" alt="" /> Festivals
                     </a>
                   </li>
                   <li>
@@ -48,7 +87,7 @@ class SearchBar extends React.Component {
                   </li>
                   <li>
                     <a href="#">
-                      <img src="img/hero/cat-4.png" alt="" /> Stand-up Shows
+                      <img src="img/hero/cat-4.png" alt="" /> StandUp
                     </a>
                   </li>
                   <li>
@@ -62,8 +101,18 @@ class SearchBar extends React.Component {
           </div>
         </div>
       </section>
+      </>
     );
-  }
 }
+
+const clearFields = (
+  setSearchText,
+  setSelectedCategory,
+  setSelectedLocation,
+) => {
+  setSearchText(undefined);
+  setSelectedCategory(undefined);
+  setSelectedLocation(undefined);
+};
 
 export default SearchBar;
