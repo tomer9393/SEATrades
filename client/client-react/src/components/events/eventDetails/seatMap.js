b83a9cd3c36b984959094eeddf4733c0d085c64c
Tomer.Israel <tomer9393@cs.colman.ac.il>
2021-05-22 13:15:51 +0300

    trying react-seatmap

--- client/client-react/src/components/events/eventDetails/seatMap.js
+++ client/client-react/src/components/events/eventDetails/seatMap.js
@@ -1,26 +1,26 @@
 import React, { useContext , useState , useEffect } from "react";
-//import Seatchart from "./seatchart";
-//require('./seatchart.js')
+import Seatmap from 'react-seatmap';
 
 
-class SeatMap extends React.Component{
-  render() {
-
+function SeatMap(){
+  //render() {
+  var rows=5;
   console.log('SeatMap Render lifecycle')
   return (
     <>
-        <div className="seatChart_content">
+        {/* <div className="seatChart_content">
             <div id="map-container"></div>
 
             <div className="seatChart_right">
                 <div id="cart-container"></div>
                 <div id="legend-container"></div>
             </div>
-        </div>
-
+        </div> */}
+        
+        <Seatmap rows={rows} maxReservableSeats={3} alpha />
     </>
     );
-  }
+  //}
 }
 
 
